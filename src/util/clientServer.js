ab7b65d2528c600a37cf2f673370a04e3fc1be60
Park Nayeon <within197@naver.com>
2024-10-30 14:40:57 +0900

    dfs 알고리즘을 사용한 데이터 가공

--- src/util/clientServer.js
+++ src/util/clientServer.js
@@ -2,14 +2,46 @@ import { SUPABASE_URL, SUPABASE_KEY } from "../various.js";
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+function dfs(visited, data, idx, currentDocuments) {
+  currentDocuments.push(data[idx]);
+  for (let i = 0; i < data.length; i++) {
+    if (!visited[i] && data[i].parent === data[idx].id) {
+      visited[i] = true;
+      dfs(
+        visited,
+        data,
+        i,
+        currentDocuments[currentDocuments.length - 1].documents
+      );
+      visited[i] = false;
+    }
+  }
+}
+
 export const getData = async () => {
   try {
     let { data: document, error } = await client
       .from("documents")
-      // .insert({  documents: [] });
+      //   .insert({
+      //   parent: 26,
+      //   documents: [],
+      //   title: "최상위다큐먼트 예시",
+      // });
       .select("*");
 
-    return await document;
+    const data = await document;
+    const madeDocuments = [];
+
+    const visited = new Array(data.length).fill(false);
+
+    for (let i = 0; i < data.length; i++) {
+      if (!visited[i] && !data[i].parent) {
+        visited[i] = true;
+        dfs(visited, data, i, madeDocuments);
+        visited[i] = false;
+      }
+    }
+    return madeDocuments;
   } catch (err) {
     console.log(err);
   }
