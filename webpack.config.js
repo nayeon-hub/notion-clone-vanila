a0f95b65a08ac03246750e7dce5a161274cfaaf4
Park Nayeon <within197@naver.com>
2024-11-15 19:09:52 +0900

    index.html 경로 수정

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "./index.html",
       filename: "index.html",
     }),
     new MiniCssExtractPlugin(),
