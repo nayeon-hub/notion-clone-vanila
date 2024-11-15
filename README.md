3c0f54daf0036df9fcb4a711816a5ee3a53eea14
Park Nayeon <within197@naver.com>
2024-11-15 20:08:13 +0900

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# notion-upgrade-project
-
-프로그래머스 노션 클로닝 프로젝트 업그레이드
-
-실행 방법 : `npx serve`
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import "./main.css";
-import App from "./App.js";
-
-const $app = document.querySelector("#app");
-new App({
-  $target: $app,
-});
