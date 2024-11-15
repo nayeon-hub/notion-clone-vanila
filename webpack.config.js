5b0b4ef48679014d5c8608cd5ecb9c5c24961f24
Park Nayeon <within197@naver.com>
2024-11-15 16:22:26 +0900

    배포 수정

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     index: "./src/main.js",
   },
   output: {
-    path: path.resolve(__dirname, "docs"),
+    path: path.resolve(__dirname, "dist"),
     filename: "[name]__bundle.js",
     publicPath: "/",
   },
