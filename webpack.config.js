d5e653728dc8ff1f61a12a38132d9d48a5fe198f
Park Nayeon <within197@naver.com>
2024-11-15 17:47:50 +0900

    경로 수정

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name]__bundle.js",
-    publicPath: "/",
+    publicPath: "/src",
   },
   module: {
     rules: [
